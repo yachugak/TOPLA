73f75fbc3857d1a07018a4c13f2a76301c2d55f9
이승준 <dltmdwns812@daum.net>
2020-10-07 15:59:26 +0900

    백엔드 스프링 템플릿 프로젝트 세팅

---
+++ topla-spring/src/test/java/com/yachugak/topla/ToplaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yachugak.topla;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToplaSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
