0361e33fc3b7b931784210c56882fe986007139f
henry174 <dltmdwns812@daum.net>
2020-10-23 21:51:21 +0900

    [12.2] todo.vue에서 작업 카드를 새로운 컴포넌트 taskCard.vue로 분리

---
+++ topla-vue/src/components/taskCard.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-card :color="bgColorByPriority[priority-1]">
+    <v-card-title>{{title}}</v-card-title>
+    <v-card-text>
+      <v-icon>mdi-clock-check-outline</v-icon>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: "taskCard",
+  data() {
+    return {
+      bgColorByPriority: [
+        "amber lighten-3",
+        "amber darken-2",
+        "amber darken-4",
+      ],
+    }
+  },
+  
+  props: {
+    title: {
+      type: String,
+      default: "제목 없는 작업"
+    },
+
+    priority: {
+      type: Number,
+      default: 1
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
