0bba42777738faf3571afa1b8e283f3f0b313ea8
romaakk <romaakk@naver.com>
2020-10-30 23:43:56 +0900

    [40.3] SchedulePresetResponseFormat 정의

---
+++ topla-spring/src/main/java/com/yachugak/topla/response/SchedulePresetResponseFormat.java
@@ -0,0 +1,15 @@
+package com.yachugak.topla.response;
+
+public class SchedulePresetResponseFormat {
+	private int[] schedulePreset = new int[7];
+
+	public int[] getSchedulePreset() {
+		return schedulePreset;
+	}
+
+	public void setSchedulePreset(int[] schedulePreset) {
+		this.schedulePreset = schedulePreset;
+	}
+	
+	
+}
