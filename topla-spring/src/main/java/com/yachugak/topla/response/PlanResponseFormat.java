e75ee5c6008b9afd87ed39ace3590fbec5969581
henry174 <dltmdwns812@daum.net>
2020-11-03 21:09:13 +0900

    [15.2] 하는 날로 보기 구현됨. 하는 날 보기 상태에서 작업을 완료 처리 했을 때 부분 처리되는 거 구현 안 됨

---
+++ topla-spring/src/main/java/com/yachugak/topla/response/PlanResponseFormat.java
@@ -0,0 +1,20 @@
+package com.yachugak.topla.response;
+
+import java.util.Date;
+
+public class PlanResponseFormat {
+	private Date doDate;
+	private int doTime;
+	public Date getDoDate() {
+		return doDate;
+	}
+	public void setDoDate(Date doDate) {
+		this.doDate = doDate;
+	}
+	public int getDoTime() {
+		return doTime;
+	}
+	public void setDoTime(int doTime) {
+		this.doTime = doTime;
+	}
+}
