98ca27866792b835fb81be6e84b3087d77007c38
henry174 <dltmdwns812@daum.net>
2020-11-08 15:39:55 +0900

    [26.2] push 알림 테스트

--- topla-vue/src/main.js
+++ topla-vue/src/main.js
@@ -5,6 +5,8 @@ import router from './router'
 import store from './store'
 import './registerServiceWorker'
 import vuetify from './plugins/vuetify';
+import firebase from "firebase/app";
+import "firebase/messaging";
 
 Vue.config.productionTip = false
 
@@ -14,3 +16,39 @@ new Vue({
   vuetify,
   render: h => h(App)
 }).$mount('#app')
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAvIewptVslnP63znkaPxQN6zKxAiadd7U",
+  authDomain: "topla-5f186.firebaseapp.com",
+  databaseURL: "https://topla-5f186.firebaseio.com",
+  projectId: "topla-5f186",
+  storageBucket: "topla-5f186.appspot.com",
+  messagingSenderId: "311735027066",
+  appId: "1:311735027066:web:2fb86c71d2238471a782c3",
+  measurementId: "G-8MPLZFWPF4"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const messaging = firebase.messaging();
+
+messaging.usePublicVapidKey("BE9PdOllh2n_xql6Mj7naem6s4D9VwWuYabNrP2AAvW3kbKyypnef47BkPbnDvrF2UWxJdAZf4zBLGuoX0sygWA");
+
+Notification.requestPermission()
+    .then((permission) => {
+      console.log('permission ', permission)
+      if (permission !== 'granted') {
+        alert('리마인더 기능을 사용하기 위해서는 알림을 허용해야 합니다.')
+      }
+    })
+
+// TODO: Send token to server for send notification
+messaging.getToken()
+    .then(console.log)
+
+// Handle received push notification at foreground
+messaging.onMessage(payload => {
+    console.log(payload)
+    alert(payload.data.message)
+})
--- topla-vue/src/sw.js
+++ topla-vue/src/sw.js
@@ -1,16 +1,32 @@
-self.addEventListener('fetch', function(e) {
-    console.log('[Service Worker] Fetched resource '+e.request.url);
-});
+// Give the service worker access to Firebase Messaging.
+// Note that you can only use Firebase Messaging here, other Firebase libraries
+// are not available in the service worker.
+importScripts('https://www.gstatic.com/firebasejs/8.0.1/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/8.0.1/firebase-messaging.js');
 
-self.onmessage = function(msg){
-    console.log(msg.data);
-    noti(msg.data)
-}
+// Initialize the Firebase app in the service worker by passing in the
+// messagingSenderId.
+firebase.initializeApp({
+    apiKey: "AIzaSyAvIewptVslnP63znkaPxQN6zKxAiadd7U",
+    projectId: "topla-5f186",
+    messagingSenderId: "311735027066",
+    appId: "1:311735027066:web:2fb86c71d2238471a782c3",
+})
 
-function noti(body){
-    new Notification("제목", {
-        body: body
-    });
+// Retrieve an instance of Firebase Messaging so that it can handle background
+// messages.
+const messaging = firebase.messaging()
 
-    setTimeout(noti, 1000);
-}
+// 백그라운드 상태에서 받은 알림 처리
+messaging.setBackgroundMessageHandler((payload) => {
+    console.log('[firebase-messaging-sw.js] Received background message ', payload)
+    // Customize notification here
+    const notificationTitle = 'Background Message Title'
+    const notificationOptions = {
+        body: 'Background Message body.',
+        icon: '/firebase-logo.png'
+    }
+
+    return self.registration.showNotification(notificationTitle,
+        notificationOptions)
+})
