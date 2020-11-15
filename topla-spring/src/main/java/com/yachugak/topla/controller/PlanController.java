8079291f88da0c45534757140fb6678423aca104
romaakk <romaakk@naver.com>
2020-11-15 20:05:24 +0900

    [48.1] 하는날 보기의 부분완료 대응 - planController

--- topla-spring/src/main/java/com/yachugak/topla/controller/PlanController.java
+++ topla-spring/src/main/java/com/yachugak/topla/controller/PlanController.java
@@ -6,11 +6,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.yachugak.topla.entity.Plan;
 import com.yachugak.topla.entity.User;
+import com.yachugak.topla.request.CheckAsFinishedRequestFormat;
 import com.yachugak.topla.service.PlanService;
+import com.yachugak.topla.service.TaskService;
 import com.yachugak.topla.service.UserService;
 
 @RestController
@@ -23,12 +29,24 @@ public class PlanController {
 	@Autowired
 	private UserService userService;
 	
+	@Autowired
+	private TaskService taskService;
+	
 	@GetMapping("")
 	@Transactional(readOnly = false)
 	public String planTest() {
 		//TODO: 나중에 모든 user 대응하게
 		User user = userService.findUserById(1L);
 		planService.plan(user, new Date());
+		
+		return "ok";
+	}
+	
+	@PutMapping("/{planUid}/finish")
+	@Transactional(readOnly = false)
+	public String updateProgress(@PathVariable("planUid") long planUid, @RequestBody CheckAsFinishedRequestFormat req) {
+		Plan targetPlan = planService.findPlanById(planUid);
+		planService.setProgress(targetPlan, req.getProgress());
 		
 		return "ok";
 	}
