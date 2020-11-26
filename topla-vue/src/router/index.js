6d3019cb28d18c651b05356bc44ee0df8fece207
hongjo <ahj0313@ajou.ac.kr>
2020-11-26 19:47:24 +0900

    [14.1] 특정 월 바로가기..파싱중

--- topla-vue/src/router/index.js
+++ topla-vue/src/router/index.js
@@ -24,6 +24,14 @@ const routes = [
     component: () => import(/* webpackChunkName: "about" */ '../views/test.vue')
   },
   {
+        path: '/test2',
+        name: 'test mode',
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () => import(/* webpackChunkName: "about" */ '../components/test.vue')
+  },
+  {
     path: '/preset',
     name: 'preset mode',
     component: () => import(/* webpackChunkName: "about" */ '../views/preset.vue')
--- topla-vue/src/views/calendar.vue
+++ topla-vue/src/views/calendar.vue
@@ -37,9 +37,42 @@
           </v-icon>
         </v-btn>
 
-        <v-toolbar-title v-if="$refs.calendar">
+        <v-menu
+            v-model="menu2"
+            :close-on-content-click="false"
+            :nudge-right="40"
+            transition="scale-transition"
+            offset-y
+            min-width="290px"
+        >
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn
+                outlined
+                class="mr-4"
+                color="grey darken-2"
+                v-model="date"
+                label="Picker in menu"
+                v-bind="attrs"
+                v-on="on"
+            >
+              {{date}}
+            </v-btn>
+          </template>
+          <v-date-picker
+              v-model="date"
+              type="month"
+              @input="setMonth"
+          ></v-date-picker>
+
+        </v-menu>
+
+        <v-btn v-if="$refs.calendar"
+               outlined
+               class="mr-4"
+               color="grey darken-2"
+               @click="setMonth">
           {{ $refs.calendar.title }}
-        </v-toolbar-title>
+        </v-btn>
 
       </v-toolbar>
       <v-btn color="primary" block
@@ -74,10 +107,13 @@ export default {
     doTasks:[],
     colors: ['blue', 'indigo', 'deep-purple', 'cyan', 'green', 'orange', 'grey darken-1'],
     taskViewMode: "dueDate",
+    date: new Date().toISOString().substr(0, 7),
+    menu2: false,
   }),
   methods: {
     setToday(){
       this.value=''
+      this.date= new Date().toISOString().substr(0, 7)
     },
     prev () {
       this.$refs.calendar.prev()
@@ -151,6 +187,20 @@ export default {
           viewMode : this.taskViewMode
         }})
     },
+
+    setMonth(){
+      let now=this.date
+      let preNow=this.$refs.calendar.title
+      let nowNum=now.substring(0,4)*12+(now.substring(5,7)*1)
+      let preNowNum=0
+      if(preNow[1]==="월")
+        preNowNum=preNow[0]*1+preNow.substring(3,7)*12
+      else
+        preNowNum=preNow.substring(0,2)*1+preNow.substring(4,8)*12
+
+      this.$refs.calendar.move(nowNum-preNowNum)
+      this.menu2=false
+    }
   },
 }
 </script>
