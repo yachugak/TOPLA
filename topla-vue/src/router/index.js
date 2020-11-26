f92ac3a3442040617383f7f11e2210b7e5cc88ac
hongjo <ahj0313@ajou.ac.kr>
2020-11-26 22:58:53 +0900

    [14.1] 원하는 달 바로가기

--- topla-vue/src/router/index.js
+++ topla-vue/src/router/index.js
@@ -23,14 +23,7 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/test.vue')
   },
-  {
-        path: '/test2',
-        name: 'test mode',
-        // route level code-splitting
-        // this generates a separate chunk (about.[hash].js) for this route
-        // which is lazy-loaded when the route is visited.
-        component: () => import(/* webpackChunkName: "about" */ '../components/test.vue')
-  },
+
   {
     path: '/preset',
     name: 'preset mode',
