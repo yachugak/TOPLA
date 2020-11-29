2a977455ec668e7d0823782a7308d67fd7e84bbf
romaakk <romaakk@naver.com>
2020-11-29 20:43:19 +0900

    [hotfix] Preset에 name 필드 추가 및 관련된 요청, 응답 포맷 대응

--- topla-spring/src/main/java/com/yachugak/topla/response/SchedulePresetResponseFormat.java
+++ topla-spring/src/main/java/com/yachugak/topla/response/SchedulePresetResponseFormat.java
@@ -1,15 +1,24 @@
 package com.yachugak.topla.response;
 
 public class SchedulePresetResponseFormat {
-	private int[] schedulePreset = new int[7];
 	private long presetUid;
-	
+	private String presetName;
+	private int[] schedulePreset = new int[7];
+
 	public long getPresetUid() {
 		return presetUid;
 	}
 
 	public void setPresetUid(long presetUid) {
 		this.presetUid = presetUid;
+	}
+
+	public String getPresetName() {
+		return presetName;
+	}
+
+	public void setPresetName(String presetName) {
+		this.presetName = presetName;
 	}
 
 	public int[] getSchedulePreset() {
