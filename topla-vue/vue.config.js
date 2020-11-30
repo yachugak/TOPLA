e2f93ee83e0f1f1e9b5b2c900aaccdb5204297ef
henry174 <dltmdwns812@daum.net>
2020-11-30 21:06:14 +0900

    [매니페스트_정리] 웹 페이지 이름과 PWA이름, 테마색, 파비콘 변경

--- topla-vue/vue.config.js
+++ topla-vue/vue.config.js
@@ -10,5 +10,17 @@ module.exports = {
     //   swSrc: 'src/firebase-messaging-sw.js',
     //   // swDest: 'service-worker.js',
     // }
+    name: "TOPLA",
+    themeColor: "#F8BBD0",
+    msTileColor: "#F8BBD0"
+  },
+
+  chainWebpack: config => {
+    config
+        .plugin('html')
+        .tap(args => {
+          args[0].title = 'TOPLA'
+          return args
+        })
   }
 }
