049994a79cc0cd63e3f499eb965cf6f2aeb54d10
이승준 <dltmdwns812@daum.net>
2020-12-04 16:44:11 +0900

    [56.1] 모든 작업 보기 화면 완성 - 완료한 작업 안 보기 버튼 추가 - 작업 정렬 기준 선택기 추가

--- topla-vue/src/views/allTask.vue
+++ topla-vue/src/views/allTask.vue
@@ -3,8 +3,12 @@
     <v-card>
       <v-card-title>전체 작업 목록</v-card-title>
       <div class="px-2">
-        <div class="w-100 flex-right">
-          <v-select class="w-50 flex-grow-0" v-model="taskPerPage" :items="taskPerPageList"></v-select>
+        <div class="w-100 flexBox">
+          <v-select class="flexItem" v-model="sortMethod" :items="sortMethodList"></v-select>
+          <v-select class="flexItem" v-model="taskPerPage" :items="taskPerPageList"></v-select>
+        </div>
+        <div>
+          <v-checkbox label="완료한 작업 안 보기" v-model="isHideDoneTask"></v-checkbox>
         </div>
         <task-card
             v-for="task in pageTaskList"
@@ -42,6 +46,8 @@ export default {
     return {
       taskPerPage: 5,
       nowPage: 1,
+      searchKeyword: "",
+
 
       callCount: 0,
       taskList: [],
@@ -51,7 +57,17 @@ export default {
           temp.push({text: `${i}개 씩 보기`, value: i});
         }
         return temp;
-      }()
+      }(),
+      sortMethodList: [
+        {text: "마감일 가까운 순", value:"dueDateIn"},
+        {text: "마감일 먼 순", value:"dueDateDe"},
+        {text: "중요도 높은 순", value:"priorityDe"},
+        {text: "중요도 낮은 순", value:"priorityIn"},
+        {text: "예상 시간 긴 순", value:"estimatedTimeDe"},
+        {text: "예상 시간 짧은 순", value:"estimatedTimeIn"},
+      ],
+      sortMethod: "dueDateIn",
+      isHideDoneTask: true
     }
   },
 
@@ -61,7 +77,7 @@ export default {
 
   computed: {
     pageLength() {
-      return Math.ceil(this.taskList.length / this.taskPerPage);
+      return Math.ceil(this.hidedTaskList.length / this.taskPerPage);
     },
 
     pagenationDisable(){
@@ -70,6 +86,16 @@ export default {
       }
 
       return false;
+    },
+
+    hidedTaskList(){
+      if(this.isHideDoneTask === false){
+        return this.taskList;
+      }
+
+      return this.taskList.filter(function(task){
+          return task.finishDate === null || task.finishDate === undefined
+      });
     },
 
     pageTaskList(){
@@ -93,12 +119,37 @@ export default {
     },
 
     sortedTaskList(){
-      let copyedTaskList = this.taskList.slice();
-      return copyedTaskList.sort(function(task1, task2){
-        let date1 = new Date(task1.dueDate);
-        let date2 = new Date(task2.dueDate);
-        return date1.getTime() - date2.getTime();
-      })
+      let copyedTaskList = this.hidedTaskList.slice();
+      return copyedTaskList.sort(this.sortFunction)
+    },
+
+    sortFunction(){
+      switch (this.sortMethod) {
+        case "dueDateIn":
+          return dueDateSort(true);
+        case "dueDateDe":
+            return dueDateSort(false);
+
+        case "priorityIn":
+          return prioritySort(true);
+        case "priorityDe":
+          return prioritySort(false);
+
+        case "estimatedTimeIn":
+          return estimateTimeSort(true);
+        case "estimatedTimeDe":
+          return estimateTimeSort(false);
+      }
+
+      throw new Error(`알 수 없는 정렬 방법:" ${this.sortMethod}`);
+    }
+  },
+
+  watch: {
+    pageLength(newVal){
+      if(this.nowPage > newVal){
+        this.nowPage = newVal;
+      }
     }
   },
 
@@ -122,6 +173,43 @@ export default {
     this.getTaskList();
   }
 }
+
+function dueDateSort(isIncrease){
+    return function(task1, task2){
+      let date1 = new Date(task1.dueDate);
+      let date2 = new Date(task2.dueDate);
+      if(isIncrease){
+        return date1.getTime() - date2.getTime();
+      }
+      else{
+        return date2.getTime() - date1.getTime();
+      }
+    }
+}
+
+function prioritySort(isIncrease){
+    return function(task1, task2){
+      let priority1 = task1.priority;
+      let priority2 = task2.priority;
+      let sub = priority1 - priority2;
+      if(isIncrease){
+        return sub;
+      }
+      return -sub;
+    }
+}
+
+function estimateTimeSort(isIncrease){
+  return function(task1, task2){
+    let estimatedTime1 = task1.estimatedTime;
+    let estimatedTime2 = task2.estimatedTime;
+    let sub = estimatedTime1 - estimatedTime2;
+    if(isIncrease){
+      return sub;
+    }
+    return -sub;
+  }
+}
 </script>
 
 <style scoped>
@@ -133,9 +221,14 @@ export default {
   width: 100%;
 }
 
-.flex-right {
+.flexBox{
   display: flex;
   flex-flow: row nowrap;
-  justify-content: flex-end;
+  justify-content: space-around;
+}
+
+.flexItem {
+  width: 45%;
+  flex-grow: 0;
 }
 </style>
